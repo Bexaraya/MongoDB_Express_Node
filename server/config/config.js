796cf6db912e2241c913e7853294485da87f682c
Bexaraya Ahmed Mohamed <bahmed@gfi.es>
2017-04-26 10:16:58 +0200

    Commit inicial

---
+++ server/config/config.js
@@ -0,0 +1,6 @@
+// Database URL
+module.exports = {
+    // Uncomment to connect with MongoDB on Cloud
+    // 'url' : 'mongodb://username:password@hakana.mongohq.com:10073/node-api'
+    'url' : 'mongodb://localhost/conferenceDB'
+};
